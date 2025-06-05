3fb0df5169e71b8c088dce5dd4fed65426d8d5ef
KiNG-HY3 <taritaelhadi@gmail.com>
2025-06-05 16:57:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,13 +6,13 @@ let guestToken = ''; //可以随便取，或者uuid生成，https://1024tools.co
 let BotToken = ''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
 let ChatID = ''; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
-let FileName = 'CF-Workers-SUB';
-let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
-let total = 99;//TB
-let timestamp = 2099-12-31;//2099-12-31
+let FileName = '国王云测试';
+let SUBUpdateTime = 6;
+let total = 24;
+let timestamp = 4102329600000;
 
 //节点链接 + 订阅链接
-let MainData = `
+let MainData = `timestamp
 https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray
 `;
 
@@ -50,7 +50,7 @@ export default {
 		const 访客订阅 = guestToken;
 		//console.log(`${fakeUserID}\n${fakeHostName}`); // 打印fakeID
 
-		let UD = Math.floor(((timestamp - Date.now()) / timestamp * total * 1099511627776) / 2);
+		let UD = Math.floor(((timestamp - Date.now()) / timestamp * 99 * 1099511627776) / 2);
 		total = total * 1099511627776;
 		let expire = Math.floor(timestamp / 1000);
 		SUBUpdateTime = env.SUBUPTIME || SUBUpdateTime;
